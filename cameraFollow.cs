d278c46e148e01ecfdd9abd0117c0b6490f2aca4
willkess <chengzhou4712@gmail.com>
2017-04-15 11:22:20 -0500

    Update cameraFollow.cs

--- cameraFollow.cs
+++ cameraFollow.cs
@@ -1,16 +1,51 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class cameraFollow : MonoBehaviour {
 
+    public GameObject c;
+    public float velocityY;
+    public float velocityX;
+    public float velocityZ;
+    public float speed;
+    Vector3 off;
+    Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-		
+        off = transform.position - c.transform.position;
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        c.transform.position = transform.position - off;
 	}
+
+    private void FixedUpdate()
+    {
+        velocityX = 0;
+        velocityZ = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            velocityZ += speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocityX -= speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            velocityZ -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocityX += speed;
+        }
+
+
+        rb.velocity = new Vector3(velocityX, velocityY, velocityZ);
+    }
 }
