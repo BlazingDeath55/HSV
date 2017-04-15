ca6e0898a4d20a6cb9db703f94437b2c5a4a4239
BlazingDeath55 <ctolbert555@gmail.com>
2017-04-15 12:18:10 -0500

    Update JunkMove.cs

--- JunkMove.cs
+++ JunkMove.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class JunkMove : MonoBehaviour {
 
-	gameObject g;
+	public GameObject p;
+    public GameObject box;
+    public Vector3 start;
+    public Vector3 v;
+    public Quaternion q;
+    public bool despawn;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        start = p.transform.position -new Vector3(0, 150, 0);
+        GetComponent<Rigidbody>().velocity = v;
+        GetComponent<Rigidbody>().rotation = q;
+        despawn = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 	
 	void OnCollisionEnter (Collision c)
 	{
-		if(c.other==g.getcomponent<Collider>())
+		if(c.other == p.GetComponent<Collider>())
 		{
-			
+			//collided with ship
 		}
+        if (c.other == box.GetComponent<Collider>())
+        {
+            despawn = true;
+        }
+
+
 	}
 }
